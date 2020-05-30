2641a62931738631806ead472069118c9a6d8984
Arkadyuti Bandyopadhyay <bandyopadhyayarkadyuti@gmail.com>
2020-05-30 14:19:36 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+
+export default function App() {
+  return (
+
+    <FlatList
+     data={[{title: 'A', key: '1'},{title: 'B', key: '2'},{title: 'C', key: '3'},{title: 'D', key: '4'},{title: 'E', key: '5'},{title: 'F', key: '6'}]}
+     renderItem={({item}) => (
+      <View style={{margin: 1,height: 225,flex: 1,justifyContent: 'center',alignItems: 'center',backgroundColor: 'maroon',color: 'white'}}>
+        <Text>{item.title}</Text>
+      </View>
+     )}
+     numColumns={2}
+     />
+  );
+}
+
+/* const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); */
